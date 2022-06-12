d5f09a72fa7818d02c7633fb5031b68a46b7e9d7
karateKime <iqbaljka@gmail.com>
2022-06-12 08:18:51 +0700

    Commit pertama Promise 152235865100-742

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(fgemosi) => {
+  let xxi = await promiseTheaterIXX();
+  let cgv = await promiseTheaterVGC();
+  
+  const gabung = xxi.concat(cgv);
+
+  return gabung.filter(element => (element.hasil === fgemosi)).length;
+}
+
+;
 
 module.exports = {
   promiseOutput,
